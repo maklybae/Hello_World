7e1e16e7282b3f25e4c869501684aa4933a0afef
Maksim Klychkov <maxim.klychkov64@gmail.com>
2023-09-06 00:26:34 +0300

    Hard code

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hi, my name is Ivan!");
+int a = 5;
+int b = 6;
+Console.WriteLine(a + b);
